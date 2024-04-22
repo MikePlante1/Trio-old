2994d4d60193eee0a44f683e1e061254ee8f930a
polscm32 <marvin-polscheit@t-online.de>
2024-04-22 13:26:09 -0400

    Refactor unChanged() in Profile Overrides

--- FreeAPS/Sources/Modules/OverrideProfilesConfig/View/OverrideProfilesRootView.swift
+++ FreeAPS/Sources/Modules/OverrideProfilesConfig/View/OverrideProfilesRootView.swift
@@ -328,14 +328,14 @@ extension OverrideProfilesConfig {
         }
 
         private func unChanged() -> Bool {
-            let isChanged = (state.percentage == 100 && !state.override_target && !state.smbIsOff && !state.advancedSettings) ||
-                (!state._indefinite && state.duration == 0) || (state.override_target && state.target == 0) ||
-                (
-                    state.percentage == 100 && !state.override_target && !state.smbIsOff && state.isf && state.cr && state
-                        .smbMinutes == state.defaultSmbMinutes && state.uamMinutes == state.defaultUamMinutes
-                )
-
-            return isChanged
+            let defaultProfile = state.percentage == 100 && !state.override_target && !state.smbIsOff && !state
+                .advancedSettings
+            let noDurationSpecified = !state._indefinite && state.duration == 0
+            let targetZeroWithOverride = state.override_target && state.target == 0
+            let allSettingsDefault = state.percentage == 100 && !state.override_target && !state.smbIsOff && state
+                .isf && state.cr && state.smbMinutes == state.defaultSmbMinutes && state.uamMinutes == state.defaultUamMinutes
+            
+            return defaultProfile || noDurationSpecified || targetZeroWithOverride || allSettingsDefault
         }
 
         private func removeProfile(at offsets: IndexSet) {
